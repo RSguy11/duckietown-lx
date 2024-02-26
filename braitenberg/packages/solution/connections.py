7b1a84a66da604d8f515f1f9846f841b3fa356cf
Sean Lawrence <naeslawrence@gmail.com>
2024-02-25 22:01:30 -0500

    Updated work for notbook 1,2,3 along with motor functions and agent

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -6,9 +6,11 @@ import numpy as np
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
+    
     # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
+
+    res[:, 120:319] = 1
+    res[:, 320:550] = -1
     # ---
     return res
 
@@ -16,7 +18,8 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    res[100:150, 100:300] = -1
-    # ---
+   
+    res[:, 321:550] = 1
+    res[:,150:320] = -1
+ 
     return res
